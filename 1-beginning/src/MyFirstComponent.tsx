7e350c38ffc9c1b7a704b97b25474a71b935e04f
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2024-01-08 12:07:53 +0100

    :construction: Added project

---
+++ 1-beginning/src/MyFirstComponent.tsx
@@ -0,0 +1,19 @@
+import "./MyFirstComponent.css";
+
+// export function MyFirstComponent() {
+//   return <h1>Hello world</h1>;
+// }
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+      <h1>Hello world - again</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse modi
+        voluptate aliquid, temporibus, sequi voluptatibus eos magni eveniet, quo
+        natus sint. Repudiandae molestias vel recusandae tempore, quisquam
+        quaerat blanditiis delectus.
+      </p>
+    </>
+  );
+};
